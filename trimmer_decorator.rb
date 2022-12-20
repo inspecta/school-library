355a87437ad99c11f5e4392bcf4756e598376dad
Derrick Mulinde <djmulinde@gmail.com>
2022-12-20 05:19:49 -0600

    Add trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,11 @@
+require './Decorator'
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length >= 10
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
