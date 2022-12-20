05ea10e74254f6caecab5d10ddd2558416715901
Derrick Mulinde <djmulinde@gmail.com>
2022-12-20 05:27:05 -0600

    Update trimmer decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,10 +2,6 @@ require './Decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name.length >= 10
-      @nameable.correct_name.slice(0, 10)
-    else
-      @nameable.correct_name
-    end
+    @nameable.correct_name.slice(0, 10)
   end
 end
