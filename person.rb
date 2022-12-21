d49a78defac4970d2364b64d44b93c3df5260d3d
Derrick <djmulinde@gmail.com>
2022-12-21 10:09:40 +0300

    Update person

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id,:rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -12,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -28,5 +29,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 end
