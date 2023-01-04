0d85d3bb9870046901a240c7c29d86e48573de8d
Derrick Mulinde <djmulinde@gmail.com>
2023-01-04 17:36:21 +0300

    Add getter and setters

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,9 @@
 require './person'
 
 class Teacher < Person
+
+  attr_accessor :age, :specialization, :name
+
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
